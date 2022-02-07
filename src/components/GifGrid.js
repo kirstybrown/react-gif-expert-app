3c09723a9966e4cf6f1559d060a9eea575c8f48c
Kirsty Brown <kirsty.a.d.brown@gmail.com>
2022-02-07 20:40:08 +0100

    added PropTypes to GifGrid component

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useFetchGifs } from '../hooks/useFetchGifs';
 import GifGridItem from './GifGridItem';
+import PropTypes from 'prop-types';
 
 const GifGrid = ({category}) => {
 
@@ -26,5 +27,9 @@ const GifGrid = ({category}) => {
         </>
   );
 };
+
+GifGrid.propTypes = {
+    category: PropTypes.string.isRequired
+}
 
 export default GifGrid;
